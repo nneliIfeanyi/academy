3165296f7377ffffe07ee028c9bf2d764b4772e5
Stanvic <stanvicbest@gmail.com>
2024-07-31 14:17:36 -0700

    git issues on cpanel

--- app/config/config.php
+++ app/config/config.php
@@ -1,13 +1,14 @@
 <?php
 // DB Params
-define("DB_HOST", "localhost");
-define("DB_USER", "stanvicc_corso");
-define("DB_PASS", "corsoAcademy2024");
-define("DB_NAME", "stanvicc_corso2");
+define('DB_HOST', 'localhost');
+define('DB_USER', 'stanvicc_bootcamp');
+define('DB_PASS', 'bootcamp2023');
+define('DB_NAME', 'stanvicc_academy');
 
 // App Root
 define('APPROOT', dirname(dirname(__FILE__)));
 // URL Root
 define('URLROOT', 'https://academy.stanvic.com.ng');
 // Site Name
-define('SITENAME', 'STANVIC ACADEMY');
+define('SITENAME', 'Web Academy');
+define('SITENAME2', 'Stanvic');
